8b91e0fad47b764df774ad9602d5f0d047496ec3
Saull Brandão <saull@outlook.com>
2022-10-31 16:39:06 -0300

    ViewChild

--- src/app/output-property/output-property.component.ts
+++ src/app/output-property/output-property.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -12,14 +20,21 @@ export class OutputPropertyComponent implements OnInit {
   @Output()
   mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput', { static: false })
+  campoValorInput!: ElementRef;
+
   increment() {
-    this.valor++;
-    this.mudouValor.emit({ novoValor: this.valor });
+    this.campoValorInput.nativeElement.value++;
+    this.mudouValor.emit({
+      novoValor: this.campoValorInput.nativeElement.value,
+    });
   }
 
   decrement() {
-    this.valor--;
-    this.mudouValor.emit({ novoValor: this.valor });
+    this.campoValorInput.nativeElement.value--;
+    this.mudouValor.emit({
+      novoValor: this.campoValorInput.nativeElement.value,
+    });
   }
 
   constructor() {}
